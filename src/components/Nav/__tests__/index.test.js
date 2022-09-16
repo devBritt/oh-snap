88346567cc1abb6e4a7db7863a0a1aaaffa5007d
Britt Carlton <brittanyrc95@gmail.com>
2022-09-16 14:37:45 -0400

    Create Nav tests

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+// remove test components from DOM after each test
+afterEach(cleanup);
+
+// component test
+describe('Nav component', () => {
+    // baseline test
+    it('renders', () => {
+        render(<Nav />);
+    });
+
+    // snapshot test
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
+
+// check for visible emoji
+describe('emoji is visible', () => {
+    it('inserts emoji into h2', () => {
+        // arrange
+        const { getByLabelText } = render(<Nav />);
+
+        // assert
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });  
+});
+
+// check for link visibility
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        // arrange
+        const { getByTestId } = render(<Nav />);
+
+        // assert
+        expect(getByTestId('home')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+})
