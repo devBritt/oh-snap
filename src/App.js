f9706fe663223067d24fa691fe186d06d0a2978e
Britt Carlton <brittanyrc95@gmail.com>
2022-09-19 13:33:54 -0400

    Implement onChange for form inputs to manage state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
     const [categories] = useState([
@@ -19,15 +20,16 @@ function App() {
 
     return (
         <div>
-        <Nav
-            categories={categories}
-            setCurrentCategory={setCurrentCategory}
-            currentCategory={currentCategory}
-        ></Nav>
-        <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
-        </main>
+            <Nav
+                categories={categories}
+                setCurrentCategory={setCurrentCategory}
+                currentCategory={currentCategory}
+            ></Nav>
+            <main>
+                <ContactForm></ContactForm>    
+                <Gallery currentCategory={currentCategory}></Gallery>
+                <About></About>
+            </main>
         </div>
     );
 }
