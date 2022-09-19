505cb91b72a6ffa94cb3ac74e705f43d4deb3fb0
Britt Carlton <brittanyrc95@gmail.com>
2022-09-19 12:21:19 -0400

    Implement PhotoList to change photos on category select

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
+import PhotoList from '../PhotoList';
 
-function Gallery(props) {
-    const currentCagetory = {
-        name: 'commercial',
-        description: 'Photos of grocery stores, food trucks and other commercial projects'
-    };
+function Gallery({ currentCategory }) {
+    const { name, description } = currentCategory;
 
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCagetory.name)}</h1>
-            <p>{currentCagetory.description}</p>
-            <div>
-                <img src={photo} alt='Fresh Produce' className='img-thumbnail mx-1' />
-            </div>
+            <h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <PhotoList currentCategory={name} />
         </section>
     );
 }
