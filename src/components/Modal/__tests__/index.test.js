9a9d57582e8fff4e2ad48a1349d30fa8a446cd8b
Britt Carlton <brittanyrc95@gmail.com>
2022-09-19 15:06:08 -0400

    Create Modal tests

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+const mockToggleModal = jest.fn();
+const currentPhoto = {
+    name: 'Yellow macaw',
+    category: 'portraits',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    index: 1
+};
+
+afterEach(cleanup);
+
+describe('Modal component', () => {
+    it('renders', () => {
+        render(<Modal currentPhoto={currentPhoto} onClose={mockToggleModal} />);
+    });
+
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<Modal currentPhoto={currentPhoto} onClose={mockToggleModal} />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
+
+describe('Click event', () => {
+    it('calls onClose handler', () => {
+        const { getByTestId } = render(<Modal onClose={mockToggleModal} currentPhoto={currentPhoto} />);
+        
+        fireEvent.click(getByTestId('closebutton'));
+
+        expect(mockToggleModal).toHaveBeenCalledTimes(1);
+    })
+})
